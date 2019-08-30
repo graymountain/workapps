cb5f8cce4adccd273afa54c2f5552e34c988460c
Bijit Soni <bijitsony@gmail.com>
2019-08-30 14:59:37 +0530

    Revise Folder tree and result path
    1. Revise folder path according to revised DPR Folder system
    2. Generate .csv without input to revised path

--- dprStatus.py
+++ dprStatus.py
@@ -1,12 +1,10 @@
-"""The purpose of the script is to
-generate a CSV report of when was the last time a DPR was updated."""
-
+#!/usr/bin/env python3.5
 import os
 import time
 from datetime import datetime
 
-dpr_filePath = "Z:\DPR\\2018"   # Path for DPR files
-os.chdir(dpr_filePath)
+"""The purpose of the script is to" +
+"generate a report of when was the time a DPR was updated."""
 
 monthDict = {"1": "Jan", "2": "Feb", "3": "Mar", "4": "Apr",
              "5": "May", "6": "Jun",
@@ -20,23 +18,22 @@ if len(todayDate) == 1:
     todayDate = "0" + todayDate
 
 
-fileList = []   # Catches the list of all the DPR fileList
-timeList = []   # Catches the last modified time stamp of all the DPR files
-employeeCount = 0   # The number of the employees
+fileList = []
+timeList = []
+employeeCount = 0
 
-catchFileList = []  # Catches the files that are not updated
-catchTimeList = []  # Catches the last modified time
-# stamp for the files that are not updated
+catchFileList = []
+catchTimeList = []
 
-dirs = os.listdir(dpr_filePath)
-for folder in dirs:
-    # For every DPR file in the folder extract last modified time
-    folderpath = dpr_filePath + "\\" + str(folder)
+path = r"Z:\DPR\2019"  # Path to the DPR folder
+dirs = os.listdir(path)
+for folder in dirs:  # For every DPR in the folder extract last modified time
+    folderpath = path + "\\"
     if os.path.isdir(folderpath):
         employeeCount += 1
         files = os.listdir(folderpath)
         for file in files:
-            if str(file)[0] != "~" and "DPR_2018" in str(file):
+            if str(file)[0] != "~" and "DPR_2019" in str(file):
                 fileList.append(file)
                 filepath = folderpath + "\\" + str(file)
                 catchTime = time.ctime(os.stat(filepath).st_mtime)
@@ -77,43 +74,32 @@ if employeeCount == len(keyList):
 else:
     endLine = "Someone has more than one copy of DPRs"
 
-# Take user input
-try:
-    name = input("Please type your user name inside quotes: ")
-    if isinstance(name, str) is True:
-        # Path of Report file to be created
-        try:
-            csvPath = "C:\Users\{}\Desktop\DPR Update Status.csv".format(name)
-            f = open(csvPath, "w+")
-            # Header for the file
-            updateTime = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-            f.write("This is an auto generated DPR update report." +
-                    "It shows when was a DPR last updated." + "\n")
-            f.write("This report was last generated on " + updateTime + "\n")
-            f.write(endLine + "\n")
-            f.write("Developed By : MSA R & D" + "\n")
-            f.write("\n")
-            # DPR report
-            for key in keyList:
-                line = key + "," + result[key]
-                f.write(line + "\n")
-            f.write("\n")
-            f.write("*******************************************************" +
-                    "\n")
-            f.write("\n")
-            f.write("An Email to be sent to following people \n")
-            f.write("\n")
-            for key in catchKeyList:
-                line = key + "," + catchResult[key]
-                f.write(line + "\n")
-            f.close()
-            print("File written successfully! Yeah!")
-        except Exception as e:
-            print("\n" +
-                  "Something went wrong! File was not written!" + "\n" +
-                  str(e))
-except Exception as e:
-    print ("Either something is wrong with the username input" +
-           " or the path of the DPR folder {}".format(e))
 
-input("Press ENTER to exit")    # To stop the CMD from closing automatically
+# Path of Report file to be created
+try:
+    csvPath = r"\\192.168.1.100\f$\DPR Status\DPR Update Status.csv"
+    f = open(csvPath, "w")
+    # Header for the file
+    updateTime = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    f.write("This is an auto generated DPR update report." +
+            "It shows when was a DPR last updated." + "\n")
+    f.write("This report was last generated on " + updateTime + "\n")
+    f.write(endLine + "\n")
+    f.write("Developed By : MSA R & D" + "\n")
+    f.write("\n")
+    # DPR report
+    for key in keyList:
+        line = key + "," + result[key]
+        f.write(line + "\n")
+    f.write("\n")
+    f.write("*******************************************************" + "\n")
+    f.write("\n")
+    f.write("An Email to be sent to following people \n")
+    f.write("\n")
+    for key in catchKeyList:
+        line = key + "," + catchResult[key]
+        f.write(line + "\n")
+    f.close()
+    print("File written successfully! Yeah!")
+except Exception as e:
+    print("\n" + "Something is wrong! File was not written!" + "\n" + str(e))
